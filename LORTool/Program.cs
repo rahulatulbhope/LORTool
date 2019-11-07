fb3e4d1e8913126a5920f1c9a83572269bd75c16
110116067 <110116067@W10-LAB1-A4>
2019-11-07 17:13:17 +0530

    Add project files.

---
+++ LORTool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LORTool
+{
+    class Program
+    {
+
+        public static List<Prof> profList = new List<Prof>();
+        static void Main(string[] args)
+        {
+            Prof p = new Prof();
+
+            using (var reader = new StreamReader(@"C:\Users\110116067\Downloads\profs.csv"))
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Console.WriteLine(values[0]);
+
+                    p.Name = values[0];
+                    p.score = Convert.ToInt32(values[1]);
+                    profList.Add(p);
+                }
+            }
+
+
+        }
+    }
+
+    public class Prof
+    {
+        public string Name { get; set; }
+        public int score { get; set; }
+    }
+}
