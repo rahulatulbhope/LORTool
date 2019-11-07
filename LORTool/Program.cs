35d994432d88f13f07c4ab5a843bfd267ada0e53
110116067 <110116067@W10-LAB1-A4>
2019-11-07 17:30:56 +0530

    new

--- LORTool/Program.cs
+++ LORTool/Program.cs
@@ -9,9 +9,10 @@ namespace LORTool
     {
 
         public static List<Prof> profList = new List<Prof>();
+        public static List<ProfGrp> profGrps = new List<ProfGrp>();
         static void Main(string[] args)
         {
-
+            int score_found = 0;
             using (var reader = new StreamReader(@"C:\Users\110116067\Downloads\profs.csv"))
             {
                 while (!reader.EndOfStream)
@@ -22,17 +23,34 @@ namespace LORTool
                     Console.WriteLine(values[0]);
                     p.Name = values[0];
                     p.score = Convert.ToInt32(values[1]);
+                    p.number = Convert.ToInt32(values[2]);
                     profList.Add(p);
                 }
             }
 
-
+            
         }
+
+        
+
+        
     }
 
     public class Prof
     {
         public string Name { get; set; }
         public int score { get; set; }
+        public int number { get; set; }
+    }
+
+    public class ProfGrp
+    {
+        public string Name1 { get; set; }
+        public string Name2 { get; set; }
+
+        public string Name3 { get; set; }
+
+
     }
 }
+
