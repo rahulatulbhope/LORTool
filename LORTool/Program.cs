ad909a86b6e470e69f995581b6d0f266df4a852c
110116067 <110116067@W10-LAB1-A4>
2019-11-07 17:40:05 +0530

    many

--- LORTool/Program.cs
+++ LORTool/Program.cs
@@ -28,7 +28,39 @@ namespace LORTool
                 }
             }
 
-            
+            for (int i = 0; i < profList.Count; i++)
+            {
+                for (int j = 0; j < profList.Count; j++)
+                {
+                    for (int k = 0; k < profList.Count; k++)
+                    {
+                        if (profList[i].Name != profList[j].Name && profList[k].Name != profList[j].Name && profList[i].Name != profList[k].Name)
+                        {
+                            int score_temp = profList[i].score + profList[j].score + profList[k].score;
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+        static Boolean nameeq(string name1, string name2, string name3)
+        {
+            if (name1 == name2 || name2 == name3 || name3 == name1)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        static Boolean no_check(int no1, int no2, int no3)
+        {
+            if (no1 == 0 || no2 == 0 || no3 == 0)
+                return false;
+            else
+                return true;
         }
 
         
